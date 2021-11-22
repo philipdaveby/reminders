ef80335065917d4b871bee0247a78cbc32256395
philipdaveby <73835849+philipdaveby@users.noreply.github.com>
2021-11-22 10:22:26 +0100

    edited backend url

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -1,5 +1,5 @@
 const config = {
-    backend_url: process.env.NODE_ENV === 'production' ? 'https://pure-shelf-04149.herokuapp.com' : 'http://localhost:8000'
+    backend_url: 'https://pure-shelf-04149.herokuapp.com'
 }
 
 export default config;
